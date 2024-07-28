7ffd59f8890874b23f6f7f80064cd7f39eec9d02
VikasSastiya <vikassastiya06@gmail.com>
2024-07-28 13:44:54 +0530

    adding new commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import Card from "./Components/Card";
 function App() {
+const data = ["Harsh","Harsh Sharma","Prachi","Avanya"];
+
   return (
     <div>
-      <Card/>
+    {data.map((elem,index)=>(
+      <div key={index} className="px-3 py-4 bg-zinc-100 rounded-md w-fit">
+        {elem}
+      </div>
+    ))}
    </div>
 );
 }
